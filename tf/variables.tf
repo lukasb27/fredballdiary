21cba73eac921181ca1fbf86499379f339bbfbf4
Lukas Ball <lukasball@live.co.uk>
2022-02-12 17:06:41 +0000

    uploading img/ folder to s3

--- tf/variables.tf
+++ tf/variables.tf
@@ -12,3 +12,8 @@ variable "r53_zone_name" {
   type        = string
   description = "the zone name for public route53 zone"
 }
+
+variable "website_files_path_images" {
+  type = string
+  description = "the path for folder that the website images are stored in"
+}
