eac47b2164da3e122aab1cdd0caf6fe252c16af2
lukasb27 <lukasball@live.co.uk>
2025-12-02 22:09:09 +0000

    feat: changing lambda to s3 source and giving CW permissions

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -1,20 +1,10 @@
-
-data "archive_file" "init" {
-  type        = "zip"
-  source_file = "${path.module}/blah.py"
-  output_path = "${path.module}/blah.zip"
-}
-
 resource "aws_lambda_function" "fred_ball_api" {
   function_name = "FredBallApi"
-
-  # The bucket name as created earlier with "aws s3api create-bucket"
-#   s3_bucket = "terraform-serverless-example"
-#   s3_key    = "v1.0.0/example.zip"
-  filename = "blah.zip"
+  s3_bucket = "lb-dumping-ground"
+  s3_key = "package.zip"
   handler = "app.main.handler"
   runtime = "python3.13"
-  architectures = ["arm64"]
+  architectures = ["x86_64"]
 
   role = "${aws_iam_role.lambda_exec.arn}"
 }
@@ -22,7 +12,7 @@ resource "aws_lambda_function" "fred_ball_api" {
 # IAM role which dictates what other AWS services the Lambda function
 # may access.
 resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_example_lambda"
+  name = "FredBallAPILambdaExecRole"
 
   assume_role_policy = <<EOF
 {
@@ -39,4 +29,33 @@ resource "aws_iam_role" "lambda_exec" {
   ]
 }
 EOF
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role_policy" "lambda_logs" {
+  name = "lambda_logs_policy"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup"
+        ]
+        Resource = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.fred_ball_api.function_name}"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${aws_lambda_function.fred_ball_api.function_name}:*"
+      }
+    ]
+  })
 }
