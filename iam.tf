a314e243b66368dc703359a1130099575914128b
lukasb27 <lukasball@live.co.uk>
2025-12-08 20:32:32 +0000

    feat: create iam permissions for front end github runner

--- iam.tf
+++ iam.tf
@@ -54,3 +54,50 @@ resource "aws_iam_role_policy" "fred_ball_api_github_actions_policy" {
   })
 
 }
+
+resource "aws_iam_role" "fred_ball_front_end_github_actions_role" {
+  name = "FredBallApiGitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.github.arn
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:lukasb27/fred-ball-frontend :*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "fred_ball_front_end_github_actions_policy" {
+  name = "FredBallApiGitHubActionsPolicy"
+  role = aws_iam_role.fred_ball_api_github_actions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+        ],
+        Resource = [
+          "${module.s3_bucket.s3_bucket_arn}/*"
+        ]
+      }
+    ]
+  })
+
+}
